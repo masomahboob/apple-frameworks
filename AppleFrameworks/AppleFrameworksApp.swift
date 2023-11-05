8e6d7753574b1d91a9aecc348a304f7223e5b1d8
Maso Mahboob <masomahboob@proton.me>
2023-11-05 23:37:37 +0430

    Project initialization.

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Mosawer Mahboob on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
